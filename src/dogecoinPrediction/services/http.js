278139299a2c9980b1c575d22ae7f43ca3df8d39
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2021-07-07 02:59:05 +0300

    Test

--- src/dogecoinPrediction/services/http.js
+++ src/dogecoinPrediction/services/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_ENDPOINT ?? "/";
+const baseURL = "/"; //process.env.REACT_APP_API_ENDPOINT ?? "/";
 
 const config = {
   baseURL,

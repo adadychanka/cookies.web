a166aafc4cdb27e8c0e2d572548282b31984d295
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2021-07-24 20:46:24 +0300

    Fix prediction video size;

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import downloadIcon from "./downloadIcon.png";
+
+export { downloadIcon };
--- src/dogecoinPrediction/assets/index.js
+++ src/dogecoinPrediction/assets/index.js
@@ -1,7 +1,6 @@
 import dogecoinPredictionArt from "./dogecoinPredictionArt.png";
 import twitterIcon from "./twitterIcon.png";
-import downloadIcon from "./downloadIcon.png";
 import merchPrint from "./merchPrint.png";
 import merchHologram from "./merchHologram.png";
 
-export { dogecoinPredictionArt, twitterIcon, downloadIcon, merchPrint, merchHologram };
+export { dogecoinPredictionArt, twitterIcon, merchPrint, merchHologram };

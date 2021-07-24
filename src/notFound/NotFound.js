7e1c960385a476ef060baf10d13a8732951014f1
Artsiom Dadychanka <artsiom.dadychanka@itechart-group.com>
2021-07-24 19:42:10 +0300

    Increase request timeout; Move some components to widget folder; Fix widget mobile layout;

--- src/notFound/NotFound.js
+++ src/notFound/NotFound.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import Background from "../components/Background";
 import Header from "../components/Header";
-import Card from "./components/Card/Card";
+import Card from "../components/Card";
 import { Button } from "../components/Button";
 
 import "./styles.css";
